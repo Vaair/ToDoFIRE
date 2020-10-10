38205a232ae4b0b6f2488ecfdcd197e06a0be574
Валерия <valeria.tarasenko.a@gmail.com>
2020-10-10 14:35:40 +0300

    Auth

--- ToDoFIRE/Controllers/TasksViewController.swift
+++ ToDoFIRE/Controllers/TasksViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -31,6 +32,15 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBAction func addTapped(_ sender: UIBarButtonItem) {
     }
     
+    @IBAction func signOut(_ sender: UIBarButtonItem) {
+        
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error.localizedDescription)
+        }
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
--- ToDoFIRE/Controllers/ViewController.swift
+++ ToDoFIRE/Controllers/ViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    let identifier = "tasksSegue"
     
     @IBOutlet weak var warnLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
@@ -17,8 +20,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        
+        warnLabel.alpha = 0
+        Auth.auth().addStateDidChangeListener { [weak self] (auth, user) in
+            if user != nil {
+                self?.performSegue(withIdentifier: (self?.identifier)!, sender: nil)
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        emailTextField.text = ""
+        passwordTextField.text = ""
     }
     
     @objc func kbDidShow(notification: Notification) {
@@ -35,11 +53,59 @@ class ViewController: UIViewController {
         scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height)
     }
     
+    func displayWarningLabel(withText text: String){
+        warnLabel.text = text
+        
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseInOut],
+                       animations: { [weak self] in
+                        self?.warnLabel.alpha = 1
+                       }) { [weak self] complete in
+            self?.warnLabel.alpha = 0
+        }
+    }
+    
     @IBAction func loginTapped(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passwordTextField.text, email != "", password != "" else {
+            displayWarningLabel(withText: "Info is incorrect")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] (user, error) in
+            if error != nil {
+                self?.displayWarningLabel(withText: "Error occured")
+                return
+            }
+            
+            if user != nil {
+                self?.performSegue(withIdentifier: (self?.identifier)!, sender: nil)
+                return
+            }
+            
+            self?.displayWarningLabel(withText: "No such user")
+        }
     }
     
     @IBAction func registerTapped(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passwordTextField.text, email != "", password != "" else {
+            displayWarningLabel(withText: "Info is incorrect")
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            
+            if error == nil {
+                if user != nil {
+                    
+                } else  {
+                    print("user is not created")
+                }
+            } else {
+                print(error!.localizedDescription)
+                
+            }
+        }
+        
     }
-    
+
 }
 
